76903348161a8ce85662a1832dd01a21526e79ac
mohammad6vakili <mohammad6vakili@gmail.com>
2023-01-22 18:56:03 +0330

    feat(add line intro): :sparkles: add line intro to line modal in lines section

--- src/pages/lines/lines.jsx
+++ src/pages/lines/lines.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import "./lines.css";
 import { Button } from "antd";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -21,6 +21,12 @@ import nextIcon from "../../assets/lines/nextIcon.svg";
 import reserveIcon from "../../assets/lines/reserveIcon.svg";
 import commentsSortIcon from "../../assets/images/postPreviewSortComments.svg";
 import Question from "../../components/question";
+import playVideoIcon from "../../assets/blog/playVideo.svg";
+import blogImage1 from "../../assets/blog/blogImage1.svg";
+import blogVideo from "../../assets/blog/blogVideo.svg";
+import blogImage4 from "../../assets/blog/blogImage4.svg";
+import blogImage5 from "../../assets/blog/blogImage5.svg";
+import blogImage6 from "../../assets/blog/blogImage6.svg";
 
 const Lines = () => {
   const history = useHistory();
@@ -166,6 +172,99 @@ const Lines = () => {
             </span>
           </div>
           <div className="lines-modal-content">
+            {/* intro */}
+            {step === 0 && (
+              <Fragment>
+                {/* title */}
+                <div className="blog-view-title" style={{ marginBottom: 8 }}>
+                  <div className="bold">
+                    معرفی لاین زیبایی اکستنشن مو و ابرو
+                  </div>
+                </div>
+                {/* content */}
+                <div className="blog-view-content">
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                  با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و
+                  مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                  تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای
+                  کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و
+                  آینده، شناخت فراوان جامعه و متخصصان را می طلبد.
+                </div>
+                {/* slider */}
+                <div className="lines-slider">
+                  <Swiper
+                    style={{
+                      width: "100%",
+                      direction: "rtl",
+                      margin: "2px 0 20px 0",
+                    }}
+                    spaceBetween={10}
+                    slidesPerView={2}
+                  >
+                    {array.map((arr, index) => (
+                      <SwiperSlide key={index}>
+                        <div href={"#"} target="_blank">
+                          <img src={blogImage1} alt="slider image" />
+                        </div>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+                {/* article */}
+                <div className="blog-view-article">
+                  <div className="bold">حفظ سلامت پوست بدن در خانه</div>
+                  <div>
+                    با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                    الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،
+                    در این صورت می توان امید داشت که تمام و دشواری موجود در
+                    ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
+                    نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته
+                    اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                  </div>
+                </div>
+                {/* video */}
+                <div className="blog-view-video">
+                  <img src={blogVideo} alt="video" />
+                  <div>
+                    <img src={playVideoIcon} alt="play" />
+                  </div>
+                </div>
+                {/* article */}
+                <div className="blog-view-article">
+                  <div className="bold">حفظ سلامت پوست بدن در خانه</div>
+                  <div>
+                    با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                    الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،
+                    در این صورت می توان امید داشت که تمام و دشواری موجود در
+                    ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
+                    نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته
+                    اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                  </div>
+                </div>
+                {/* image grid */}
+                <div className="blog-view-grid">
+                  <div>
+                    <img src={blogImage4} alt="post" />
+                    <img src={blogImage5} alt="post" />
+                  </div>
+                  <div>
+                    <img src={blogImage6} alt="post" />
+                  </div>
+                </div>
+                {/* article */}
+                <div className="blog-view-article">
+                  <div className="bold">حفظ سلامت پوست بدن در خانه</div>
+                  <div>
+                    با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                    الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،
+                    در این صورت می توان امید داشت که تمام و دشواری موجود در
+                    ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
+                    نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته
+                    اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                  </div>
+                </div>
+              </Fragment>
+            )}
             {/* posts */}
             {step === 1 && (
               <div className="home-posts">
